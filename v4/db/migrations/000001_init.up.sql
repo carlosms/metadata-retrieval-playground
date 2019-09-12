199192b5de129c55ad969d7b74039861a22ac02d
Carlos Martín <carlos.martin.sanchez@gmail.com>
2019-09-12 18:09:20 +0100

    Add index on versions columns

--- v4/db/migrations/000001_init.up.sql
+++ v4/db/migrations/000001_init.up.sql
@@ -13,6 +13,8 @@ CREATE TABLE IF NOT EXISTS repositories_versioned (
   UNIQUE(database_id, created_at, description, owner, name)
 );
 
+CREATE INDEX IF NOT EXISTS versions ON repositories_versioned (versions);
+
 CREATE TABLE IF NOT EXISTS issues_versioned (
   pk                SERIAL PRIMARY KEY,
   versions          text ARRAY,
@@ -27,6 +29,8 @@ CREATE TABLE IF NOT EXISTS issues_versioned (
   UNIQUE(database_id, title, body, number, repository_owner, repository_name)
 );
 
+CREATE INDEX IF NOT EXISTS versions ON issues_versioned (versions);
+
 CREATE TABLE IF NOT EXISTS issue_comments_versioned (
   pk                SERIAL PRIMARY KEY,
   versions          text ARRAY,
@@ -40,5 +44,7 @@ CREATE TABLE IF NOT EXISTS issue_comments_versioned (
 
   UNIQUE(database_id, author, body, repository_owner, repository_name, issue_number)
 );
+
+CREATE INDEX IF NOT EXISTS versions ON issue_comments_versioned (versions);
 
 COMMIT;
